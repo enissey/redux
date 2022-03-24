07ecf0bcdfb40955bd07c99f4d4a5c2e80c0c3a5
yassine <yassinechatti0123@gmail.com>
2022-03-24 16:15:45 +0100

    first

---
+++ src/Components/Header.js
@@ -0,0 +1,45 @@
+import { addTask } from '../JS/Actions/actionTask'
+import React, { useState } from 'react'
+import { Card, Form, InputGroup, Button, FormControl } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+
+const Header = () => {
+    const [myInput,setMyInput] = useState("")
+    const dispatch = useDispatch()
+    const add = e => {
+        e.preventDefault();
+        if (myInput) {
+            dispatch(addTask({description: myInput,id: Math.random(),isDone: false}))
+            setMyInput("")
+        }
+        else {
+            alert("type something")
+        }
+    }
+  return (
+    <Card bg="primary">
+    <Card.Body >
+        <h1 className={"text-white"}>To-Do-App !</h1>
+
+        <Form onSubmit={add}>
+            <Form.Group>
+                <InputGroup className="mb-3">
+                    <FormControl
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                        onChange={(e) => setMyInput(e.target.value)}
+                        value={myInput}
+                    />
+                    <Button variant="success" onClick={add}>
+                        +
+                    </Button>
+
+                </InputGroup>
+            </Form.Group>
+        </Form>
+    </Card.Body>
+</Card>
+  )
+}
+
+export default Header
